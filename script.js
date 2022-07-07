d9ba961427906d3dc8e7d4cbf18df7a80b48ad2d
A-Tani-tmc <105775431+A-Tani-tmc@users.noreply.github.com>
2022-07-08 00:41:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ async function RenderNextSentence() { //非同期関数
 }
 
 let startTime; //文字が新たに表示された時の時刻
-let originTime = 10; //タイマーの最大値
+let originTime = 30; //タイマーの最大値
 
 
 /* カウントアップを開始する */////////////////////////////////
