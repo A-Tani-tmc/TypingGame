3c0dade9e42306fcb3be6d709c83bd8061c1bf82
A-Tani-tmc <105775431+A-Tani-tmc@users.noreply.github.com>
2022-07-10 01:16:08 +0900

    Update chart.js

--- chart.js
+++ chart.js
@@ -1,25 +1,40 @@
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
+
+
 let ctx = document.getElementById("myPieChart");
-let numOfMiss = document.getElementById("miss").innerText;
-let numOfCorrect = document.getElementById("total").innerText - numOfMiss;
+const typeInputElement2 = document.getElementById("typeInput"); //タイピングする領域の要素
 
-console.log("AAAAA", numOfCorrect, numOfMiss);
+drawPieChart ();
 
-let myPieChart = new Chart(ctx, {
-  type: 'pie',
-  data: {
-    labels: ["SUCCESS", "MISS"],
-    datasets: [{
-        backgroundColor: [
-            "rgb(0, 195, 254)",
-            "red"
-        ],
-        data: [numOfCorrect, numOfMiss]
-    }]
-  },
-  options: {
-    title: {
-      display: true,
-      text: '正/誤割合'
-    }
-  }
+/* inputテキスト入力。合っているかどうかの判定 */////////////////////////////////
+typeInputElement2.addEventListener("input", () => { //打ち込まれる毎に第二引数の関数が呼び出される
+    drawPieChart ();
 });
+
+function drawPieChart (){
+
+    let numOfMiss = Number(document.getElementById("miss").innerText);
+    let numOfCorrect = Number(document.getElementById("total").innerText - numOfMiss);
+
+    ctx = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ["SUCCESS", "MISS"],
+            datasets: [{
+                backgroundColor: [
+                    "rgb(0, 195, 254)",
+                    "red"
+                ],
+                data: [numOfCorrect, numOfMiss]
+            }]
+        },
+        options: {
+            responsive: false,
+            title: {
+                display: true,
+                text: '正/誤割合'
+            }
+        }
+    });
+}
